6a79ed70291e78ac63e20f9bc08523e3b287a8bd
Ekaterina Losik <nor11324@gmail.com>
2022-07-08 17:27:08 +0200

    make artworks movable, adjust tutorial, fix gallery respawning issue

--- Assets/Scripts/VR/Teleporter.cs
+++ Assets/Scripts/VR/Teleporter.cs
@@ -8,57 +8,43 @@ using UnityEngine.InputSystem.Interactions;
 using UnityEngine.XR.Interaction.Toolkit;
 
 
-    public class VRCustomTeleporter : MonoBehaviour
+    public class Teleporter : MonoBehaviour
     {
+        public GameObject TeleportRay;
+        public TeleportationArea TPArea;
         [SerializeField] private InputActionReference teleportInput;
-        [SerializeField] private GameObject teleportRay;
         [SerializeField] private TeleportationProvider teleportationProvider;
 
-        private bool wallWasHit;
-
-        private void Start()
-        {
-            var areas = FindObjectsOfType<TeleportationArea>();
-            foreach (var area in areas)
-            {
-                area.teleportationProvider = teleportationProvider;
-            }
-
-            if (teleportRay != null)
-                teleportRay.SetActive(false);
+        private void Start(){   
+            TPArea.teleportationProvider = teleportationProvider;
+            if (TeleportRay != null)
+                TeleportRay.SetActive(false);
         }
 
         private void OnEnable()
         {
             teleportInput.action.Enable();
-
             teleportInput.action.started += ButtonPressed;
-            teleportInput.action.canceled += ButtonReleased;
-            
+            teleportInput.action.canceled += ButtonReleased;  
         }
 
         private void OnDisable()
         {
             teleportInput.action.Disable();
-
             teleportInput.action.started -= ButtonPressed;
             teleportInput.action.canceled -= ButtonReleased;
         }
 
         private void ButtonPressed(InputAction.CallbackContext obj)
         {
-            teleportRay.SetActive(true);
+            TeleportRay.SetActive(true);
         }
 
        
-
         private async void ButtonReleased(InputAction.CallbackContext obj)
         {
-            // safety wait until player has teleported
             await Task.Delay(100);
-
-            // Deactivate ray again
-            teleportRay.SetActive(false);
+            TeleportRay.SetActive(false);
         }
 
     }
