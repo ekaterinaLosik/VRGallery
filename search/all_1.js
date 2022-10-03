10cdc1a8ccfd529fcda3aa4ae986fa3b2511cea1
ekaterinaLosik <ekaterinaLosik@users.noreply.github.com>
2022-10-03 16:01:59 +0000

    deploy: 8578056ba1d4d982d297d59424c254f293a78bf3

--- search/all_1.js
+++ search/all_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['activationmode_0',['ActivationMode',['../class_guide_voice.html#a3a5574ba7a5cabeecdb6b78686d9ab51',1,'GuideVoice']]],
-  ['activationmode_1',['activationMode',['../class_guide_voice.html#aa40e650b080954479ce56732ba980ee4',1,'GuideVoice']]],
+  ['activationmode_0',['activationMode',['../class_guide_voice.html#aa40e650b080954479ce56732ba980ee4',1,'GuideVoice']]],
+  ['activationmode_1',['ActivationMode',['../class_guide_voice.html#a3a5574ba7a5cabeecdb6b78686d9ab51',1,'GuideVoice']]],
   ['addtofilterlist_2',['AddToFilterList',['../class_keyword_manager.html#acca619072138c06824a89451d39aaabc',1,'KeywordManager']]],
   ['animatehand_3',['AnimateHand',['../class_hand.html#adf592c0c0c9799592378c040122afdf5',1,'Hand']]],
   ['appstate_4',['AppState',['../class_app_state.html',1,'']]],
